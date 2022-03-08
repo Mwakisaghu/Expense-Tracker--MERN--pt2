e5165ea9ff13c0cb79806293c152d8a4a3bb066e
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-08 13:55:41 +0300

    This shows addTransaction & deleteTransaction methods

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,5 +2,19 @@
 //@routes     GET/api/v1/transactions
 //access      Public
 exports.getTransactions = (req, res, next) => {
-  res.send('GET Transactions');
+  res.send('GET Transaction');
+};
+
+//@desc       Add transaction
+//@routes     POST/api/v1/transactions
+//access      Public
+exports.addTransaction = (req, res, next) => {
+  res.send('POST Transaction');
+};
+
+//@desc       Delete transaction
+//@routes     DELETE/api/v1/transactions/:id
+//access      Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send('DELETE Transaction');
 };
