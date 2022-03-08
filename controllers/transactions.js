e79e896a4b0c0f82d21fa58e148046ca2621cc90
mwakisaghu <harrisonchambela2016@gmail.com>
2022-03-08 21:25:00 +0300

    This shows getTransactions method to get all transaction history

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,8 +3,21 @@ const Transaction = require('../models/Transaction');
 //@desc       Get all transactions
 //@routes     GET/api/v1/transactions
 //access      Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET Transaction');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      sucess: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.send(500).json({
+      sucess: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 //@desc       Add transaction
